b9179f627c5b4e37eee780c9309b19490f955ae7
Gary Chang <dreamcryersocial@outlook.com>
2015-06-25 14:32:17 -0700

    Use Log npm package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-﻿var express = require('express');
+﻿var log = require('log');
+var express = require('express');
 var router = express.Router();
+var logger = new log('debug', process.stdout);
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    //console.log("Rendering Index Page...");
+    logger.debug("Rendering index...");
     res.render('index', { title: 'Express' });
 });
 
