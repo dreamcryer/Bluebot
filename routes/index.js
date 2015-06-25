a956eac6c2f55f6e43ac43a6a8959314e912750f
Gary Chang <dreamcryersocial@outlook.com>
2015-06-25 15:10:10 -0700

    Use Log NPM to log in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-﻿var express = require('express');
+﻿var log = require('log');
+var express = require('express');
 var router = express.Router();
+var logger = new log('debug', process.stdout);
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    process.stdout.write("Rendering index...");
+    logger.debug("Rendering index...");
     res.render('index', { title: 'Express' });
 });
 
--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-//var app = require('./app.js');
-//app.set('port', process.env.PORT || 1337);
+var app = require('./app.js');
+app.set('port', process.env.PORT || 1337);
 
-//var server = app.listen(app.get('port'), function () {
+var server = app.listen(app.get('port'), function () {});
 
-//});
-
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function (req, res) {
-    process.stdout.write("Test 123");
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Azure Node\n');
-}).listen(port);
+//var http = require('http')
+//var port = process.env.PORT || 1337;
+//http.createServer(function (req, res) {
+//    process.stdout.write("Test 123");
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello Azure Node\n');
+//}).listen(port);
