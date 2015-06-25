948eb345af760283476758eb814843c3d0d1a671
Gary Chang <dreamcryersocial@outlook.com>
2015-06-25 14:55:19 -0700

    Simple server.js console log test

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-var app = require('./app.js');
-app.set('port', process.env.PORT || 1337);
+//var app = require('./app.js');
+//app.set('port', process.env.PORT || 1337);
 
-var server = app.listen(app.get('port'), function () {
+//var server = app.listen(app.get('port'), function () {
 
-});
+//});
 
-//var http = require('http')
-//var port = process.env.PORT || 1337;
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello Azure Node\n');
-//}).listen(port);
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function (req, res) {
+    console.log("test");
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Azure Node\n');
+}).listen(port);
