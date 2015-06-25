d92274d9915b18152b49f4a54115f44d431af0ac
Gary Chang <dreamcryersocial@outlook.com>
2015-06-25 14:34:40 -0700

    Use process.stdout for logging

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-﻿var log = require('log');
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
-var logger = new log('debug', process.stdout);
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    logger.debug("Rendering index...");
+    process.stdout.write("Rendering index...");
     res.render('index', { title: 'Express' });
 });
 
