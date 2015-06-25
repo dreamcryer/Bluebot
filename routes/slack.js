c671ebbe2d8284486bc9c7e1508631af031abf5a
Gary Chang <dreamcryersocial@outlook.com>
2015-06-25 15:50:20 -0700

    Add /slack/cmd post method

---
+++ routes/slack.js
@@ -0,0 +1,11 @@
+﻿var log = require('log');
+var express = require('express');
+var router = express.Router();
+var logger = new log('debug', process.stdout);
+
+router.post('/cmd', function (req, res) {
+    logger.debug(req.body);
+    res.send('test');
+});
+
+module.exports = router;
