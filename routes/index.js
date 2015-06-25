0da937c681c09ccb3d3569b2e4f90a0366f5dd65
Gary Chang <dreamcryersocial@outlook.com>
2015-06-25 13:31:18 -0700

    Test console log

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-﻿var express = require('express');
+﻿var azure = require('azure');
+var express = require('express');
 var router = express.Router();
+
+var tableService = azure.createTableService("k2i9w+R9BcOi0wOnHBVNWGMuI+MN8rLQzhp3hnpgmysX5qaJfmuur9WJ5/DGNl0rL52yY5k/kMKKv2huJwvdHA==");
 
 /* GET home page. */
 router.get('/', function (req, res) {
+    tableService.logger = new azure.Logger(azure.Logger.LogLevels.DEBUG);
+    tableService.logger.log("DEBUG", "Rendering Index page...")
     res.render('index', { title: 'Express' });
 });
 
