07e532613bda16bb4d386b28ec117657a7733e9a
Gary Chang <dreamcryersocial@outlook.com>
2015-06-25 13:35:34 -0700

    Remove console log

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
-﻿var azure = require('azure');
-var express = require('express');
+﻿var express = require('express');
 var router = express.Router();
-
-var tableService = azure.createTableService("k2i9w+R9BcOi0wOnHBVNWGMuI+MN8rLQzhp3hnpgmysX5qaJfmuur9WJ5/DGNl0rL52yY5k/kMKKv2huJwvdHA==");
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    tableService.logger = new azure.Logger(azure.Logger.LogLevels.DEBUG);
-    tableService.logger.log("DEBUG", "Rendering Index page...")
+    //console.log("Rendering Index Page...");
     res.render('index', { title: 'Express' });
 });
 
