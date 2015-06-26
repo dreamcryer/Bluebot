47508e5b4fa70aeed2df76fde02fa6234294604a
Gary Chang <dreamcryersocial@outlook.com>
2015-06-25 22:22:11 -0700

    Get blob segments for Slack cmd listblob

--- routes/slack.js
+++ routes/slack.js
@@ -1,4 +1,5 @@
-﻿var log = require('log');
+﻿var azure = require('azure');
+var log = require('log');
 var express = require('express');
 var router = express.Router();
 var logger = new log('debug', process.stdout);
@@ -10,7 +11,27 @@ router.post('/cmd', function (req, res) {
         var cmdTokens = slackRequest.split(' ');
         switch (cmdTokens[0]) {
             case 'listblob':
-                res.send('Blob files:');
+                var blob = azure.createBlobService('blubotlog', 'k2i9w+R9BcOi0wOnHBVNWGMuI+MN8rLQzhp3hnpgmysX5qaJfmuur9WJ5/DGNl0rL52yY5k/kMKKv2huJwvdHA==');
+                blob.listBlobsSegmented(
+                    'agaveanomalyml',
+                    null,
+                    function (error, result, response) {
+                        var fileList = '';
+                        if (!error) {
+                            for (var i in result.entries) {
+                                fileList += result.entries[i].name + ', ';
+                            }
+                            
+                            if (fileList != '') {
+                                fileList = fileList.substring(0, fileList.length - 2);
+                            }
+                        }
+                        else {
+                            logger.debug('Error getting segments');
+                        }
+                        res.send('Blob files: ' + fileList);
+                    }
+                );
                 break;
             case 'predict':
                 res.send('Predict new data:');
@@ -19,7 +40,7 @@ router.post('/cmd', function (req, res) {
                 res.send('Report last run:');
                 break;
             default:
-                res.send('Hi, ' + req.body.user_name + ' from ' + '#' + req.body.channel_name + 'on ' + req.body.team_domain);
+                res.send('Hi, ' + req.body.user_name + ' from ' + '#' + req.body.channel_name + ' on ' + req.body.team_domain);
                 break;
         }
     }
