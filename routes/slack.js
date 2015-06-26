65d8c62c2b422246e85376fa3aae1571fac829be
Gary Chang <dreamcryersocial@outlook.com>
2015-06-25 20:54:29 -0700

    Slack cmd skeleton

--- routes/slack.js
+++ routes/slack.js
@@ -4,8 +4,24 @@ var router = express.Router();
 var logger = new log('debug', process.stdout);
 
 router.post('/cmd', function (req, res) {
-    logger.debug(req.body);
-    res.send('test');
+    if (req.body != null || req.body != "") {
+        var slackRequest = req.body.text.trim();
+        var cmdTokens = slackRequest.split(' ');
+        switch (cmdTokens[0]) {
+            case 'list':
+                res.send('Blob files:');
+                break;
+            case 'predict':
+                res.send('Predict new data:');
+                break;
+            case 'report':
+                res.send('Report last run:');
+                break;
+            default:
+                res.send('Hi, ' + req.body.user_name + 'from ' + '#' + req.body.channel_name + 'on ' + req.body.team_domain);
+                break;
+        }
+    }
 });
 
 module.exports = router;
